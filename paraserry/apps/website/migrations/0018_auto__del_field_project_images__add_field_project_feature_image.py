7409ab189363f77ddc450d12f8e645864dca77ad
Sara Perry <paraserry@gmail.com>
2014-12-28 14:54:52 -0500

    adding migration

---
+++ paraserry/apps/website/migrations/0018_auto__del_field_project_images__add_field_project_feature_image.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Deleting field 'Project.images'
+        db.delete_column(u'website_project', 'images')
+
+        # Adding field 'Project.feature_image'
+        db.add_column(u'website_project', 'feature_image',
+                      self.gf('django.db.models.fields.related.ForeignKey')(to=orm['media.Image'], null=True, blank=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Adding field 'Project.images'
+        db.add_column(u'website_project', 'images',
+                      self.gf('django.db.models.fields.files.ImageField')(default=1, max_length=100),
+                      keep_default=False)
+
+        # Deleting field 'Project.feature_image'
+        db.delete_column(u'website_project', 'feature_image_id')
+
+
+    models = {
+        u'media.document': {
+            'Meta': {'object_name': 'Document'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['media.Image']", 'null': 'True', 'blank': 'True'}),
+            'media_file': ('django.db.models.fields.files.FileField', [], {'max_length': '100', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        u'media.image': {
+            'Meta': {'object_name': 'Image'},
+            'caption': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'credit': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        u'website.contentblock': {
+            'Meta': {'object_name': 'ContentBlock'},
+            'content': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'documents': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'related_name': "'contentblock_document'", 'null': 'True', 'symmetrical': 'False', 'to': u"orm['media.Document']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'images': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'related_name': "'contentblock_image'", 'null': 'True', 'symmetrical': 'False', 'to': u"orm['media.Image']"}),
+            'order': ('django.db.models.fields.PositiveSmallIntegerField', [], {}),
+            'project': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['website.Project']", 'null': 'True', 'blank': 'True'})
+        },
+        u'website.linkitem': {
+            'Meta': {'ordering': "['order']", 'object_name': 'LinkItem'},
+            'display_name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'hide': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'order': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['website.LinkItem']", 'null': 'True', 'blank': 'True'}),
+            'path': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'txtid': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
+            'url': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'})
+        },
+        u'website.project': {
+            'Meta': {'object_name': 'Project'},
+            'about': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'client': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'feature_image': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['media.Image']", 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'launch_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'main_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
+            'order': ('django.db.models.fields.PositiveSmallIntegerField', [], {}),
+            'project_title': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'published': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'tags': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'related_name': "'project_tags'", 'null': 'True', 'symmetrical': 'False', 'to': u"orm['website.Tag']"}),
+            'txtid': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        u'website.resumeitem': {
+            'Meta': {'object_name': 'ResumeItem'},
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'employer': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'end_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'hide': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'start_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'})
+        },
+        u'website.resumeitemdetail': {
+            'Meta': {'object_name': 'ResumeItemDetail'},
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'hide': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'order': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['website.ResumeItem']"})
+        },
+        u'website.tag': {
+            'Meta': {'ordering': "['title']", 'object_name': 'Tag'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['website.Tag']", 'null': 'True', 'blank': 'True'}),
+            'published': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'slug': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        }
+    }
+
+    complete_apps = ['website']
