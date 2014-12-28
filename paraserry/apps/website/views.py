92a361cebc98026512b0c0cf2c5e849e34fbe4f8
Sara Perry <paraserry@gmail.com>
2014-12-28 00:07:22 -0500

    more styling

--- paraserry/apps/website/views.py
+++ paraserry/apps/website/views.py
@@ -7,6 +7,19 @@ from django.views.generic import ListView, DetailView, TemplateView
 from .models import *
 
 
+class ProjectDetail(DetailView):
+    """
+    ================
+    Project Detail View
+    ================
+
+    Text Detail View extended from the Django Generic DetailView. The Queryset
+    has been overridden to only display published articles.
+    """
+
+    model = Project
+    template_name = "partials/project_detail.html"
+
 class HomePageView(TemplateView):
 
     template_name = "home.html"
@@ -17,6 +30,7 @@ class HomePageView(TemplateView):
         resume_list = ResumeItem.objects.filter(hide=False).order_by("-start_date")
         project_list = Project.objects.filter(published=True)\
             .order_by('order')
+
 
         context['resume_list'] = resume_list
         context['project_list'] = project_list
--- paraserry/urls.py
+++ paraserry/urls.py
@@ -17,7 +17,7 @@ urlpatterns = patterns('',
     (r'^grappelli/', include('grappelli.urls')), # grappelli URLS
     (r'^admin/',  include(admin.site.urls)), # admin site
 
-    #url(r'^projects/(?P<pk>[\w-]+)/$', ProjectDetail.as_view(), name='project'),
+    url(r'^projects/(?P<pk>[\w-]+)/$', ProjectDetail.as_view(), name='project-detail'),
     #url(r'^projects/$', ProjectList.as_view(), name = 'projects'),
 
     url(r'^$', HomePageView.as_view(template_name='home.html'), name="home"),
